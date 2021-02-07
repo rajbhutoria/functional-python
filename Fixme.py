a925573240fbc20e9d71e40eb84df9c029376ade
rajbhutoria <rbhutoria22@cmc.edu>
2021-02-07 01:09:54 -0800

    Fix five functions in Fixme.py

--- Fixme.py
+++ Fixme.py
@@ -2,13 +2,14 @@
 '''
 Your solution for each function should be only a single line long.
 In particular, you may not use any loops when implementing these functions;
-instead, you must use either the map and filter functions or list comprehensions.
+instead, you must use either the map and filter functions or
+list comprehensions.
 '''
+
 
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
-
     >>> evens(10)
     [0, 2, 4, 6, 8, 10]
     >>> evens(11)
@@ -20,12 +21,17 @@ def evens(n):
     >>> evens(-1)
     []
     '''
+    list_evens = []
+    for x in range(n+1):
+        if x % 2 == 0:
+            list_evens.append(x)
+    return list_evens
 
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
-
+    Returns a list of all numbers from 0 to n inclusive
+    that contain the digit 3.
     >>> threes(2)
     []
     >>> threes(3)
@@ -37,11 +43,17 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
+    list_threes = []
+    for x in range(1, n+1):
+        if '3' in str(x):
+            list_threes.append(x)
+    return list_threes
 
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 4 characters long.
+    Returns a list of all words in the input text that are
+    less than 4 characters long.
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -55,7 +67,12 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-    return [ word for word in text.split() if len(word)<=4 ]
+    list_small_words = text.split(" ")
+    list_success = []
+    for x in list(list_small_words):
+        if len(x) >= 1 and len(x) <= 4:
+            list_success.append(x)
+    return list_success
 
 
 def squares(n):
@@ -72,11 +89,16 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
+    list_squares = []
+    for x in range(1, n+1):
+        list_squares.append(x**2)
+    return list_squares
 
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings, returns a list of the lengths of the
+    corresponding strings.
 
     >>> lengths([])
     []
@@ -85,3 +107,7 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    list_lengths = []
+    for x in list(strings):
+        list_lengths.append(len(x))
+    return list_lengths
